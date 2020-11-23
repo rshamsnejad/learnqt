02b09ae47f482949573d31490bdf18d51e41626a
Robin SHAMSNEJAD <rsham@free.fr>
2020-11-23 15:41:57 +0100

    Customize push button

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@ int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
 	
-	QPushButton button("Hello World !");
+	QPushButton button;
+	
+	button.setText("Click me !!");
+	button.setToolTip("Believe it, you can click me");
+	QFont font ("Comic Sans", 47, QFont::Thin, true);
+	button.setFont(font);
+	
 	button.show();
 
 	return app.exec();
